1552e4dca80bbd5fe1d95e1cb764eb68b2ee6e59
ivotai <ivotai@163.com>
2018-12-01 16:01:29 +0800

    暂时不做建筑

--- app/src/main/java/com/unicorn/signboard/input/InputPagerAdapter.kt
+++ app/src/main/java/com/unicorn/signboard/input/InputPagerAdapter.kt
@@ -11,14 +11,14 @@ class InputPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
     override fun getItem(position: Int): Fragment {
         return when (position) {
             0 -> AddMerchantFra()
-            1 -> AddMerchantFra()
-            2 -> AddGroundFra()
+//            1 -> AddMerchantFra()
+            1 -> AddGroundFra()
             else -> null!!
         }
     }
 
-    override fun getCount() = 3
+    override fun getCount() = 2
 
-    override fun getPageTitle(position: Int) = listOf("墙面", "建筑", "地面")[position]
+    override fun getPageTitle(position: Int) = listOf("墙面", "地面")[position]
 
 }
